64fa1afce9f8229397b8abda592e2588997ba359
BytelyPlay <universalnebula2024@outlook.com>
2025-09-28 13:56:54 +0200

    Alright time to test

---
+++ src/main/java/org/bytelyplay/pvptoggle/listeners/CommandRegistrationListener.java
@@ -0,0 +1,44 @@
+package org.bytelyplay.pvptoggle.listeners;
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
+import net.minecraft.util.CommonColors;
+import net.minecraft.world.entity.player.Player;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import org.bytelyplay.pvptoggle.Main;
+import org.bytelyplay.pvptoggle.utils.Messages;
+
+import java.util.UUID;
+
+@EventBusSubscriber(modid = Main.MODID)
+public class CommandRegistrationListener {
+    @SubscribeEvent
+    public static void onCommandRegistrationEvent(RegisterCommandsEvent event) {
+        // Normally i'd make this modular but it is not worth it for one command...
+        event.getDispatcher().register(
+                LiteralArgumentBuilder.<CommandSourceStack>literal("pvp")
+                .executes((ctx) -> {
+                    CommandSourceStack source = ctx.getSource();
+                    if (source.isPlayer()) {
+                        Player sender = source.getPlayerOrException();
+                        UUID senderUUID = sender.getUUID();
+
+                        PVPManager.setPVP(senderUUID,
+                                PVPManager.getPVP(senderUUID));
+                        boolean pvpState = PVPManager.getPVP(senderUUID);
+                        if (pvpState) {
+                            sender.sendSystemMessage(Messages.ENABLED_PVP);
+                        } else {
+                            sender.sendSystemMessage(Messages.DISABLED_PVP);
+                        }
+                    } else {
+                        source.sendSystemMessage(Messages.ONLY_PLAYERS_CAN_EXECUTE);
+                    }
+                    return 1;
+                })
+        );
+    }
+}
--- src/main/java/org/bytelyplay/pvptoggle/utils/Messages.java
+++ src/main/java/org/bytelyplay/pvptoggle/utils/Messages.java
@@ -7,4 +7,13 @@ public class Messages {
     public static Component ATTACK_DENIED = Component
             .literal("You can't attack this player because either one of your PVPs are disabled.")
             .withColor(CommonColors.RED);
+    public static Component ONLY_PLAYERS_CAN_EXECUTE = Component
+            .literal("Only players can execute this command.")
+            .withColor(CommonColors.YELLOW);
+    public static Component ENABLED_PVP = Component
+            .literal("Enabled PVP")
+            .withColor(CommonColors.RED);
+    public static Component DISABLED_PVP = Component
+            .literal("Disabled PVP")
+            .withColor(CommonColors.RED);
 }
